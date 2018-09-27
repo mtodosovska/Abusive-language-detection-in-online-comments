912ec7912809c80c7d83e40b653e0150bd40dea3
Marija <mtodosovska@gmail.com>
2018-09-28 01:47:49 +0200

    Added offensiveness score.

--- src/figuring_it_out.py
+++ src/figuring_it_out.py
@@ -1,19 +1,25 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import datetime
+
+
 from sklearn.base import TransformerMixin
 from sklearn.pipeline import Pipeline
+from sklearn.pipeline import FeatureUnion
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import BayesianRidge
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import FeatureUnion
-from sklearn.feature_extraction.text import TfidfVectorizer
-import datetime
-
-from sqlalchemy.orm.attributes import ScalarAttributeImpl
 
 
 class SimpleTransformerGeneral(TransformerMixin):
@@ -28,80 +34,84 @@ class SimpleTransformerGeneral(TransformerMixin):
         return self
 
 
-class SimpleTransformerComments(TransformerMixin):
+def get_data(n):
+    print('Getting data')
+    print(datetime.datetime.now())
+    scores = pd.read_csv('../features/scores.csv', encoding='latin1').drop('Unnamed: 0', axis=1).iloc[0:n]
+    pos_tags = pd.read_csv('../features/pos_ngrams.csv', encoding='latin1').iloc[0:n]
+    scores = scores.merge(pos_tags, on='rev_id')
+    del pos_tags
 
-    def transform(self, X, **transform_params):
-        # sentences.append(" ".join(str(x) for x in comment))
-        # X['comment'] = X['comment'].apply(lambda x: x.replace("NEWLINE_TOKEN", " "))
-        # X['comment'] = X['comment'].apply(lambda x: x.replace("TAB_TOKEN", " "))
-        # ls = X['words']
-        X['words'] = X['comment'].apply(lambda x: " ".join(str(y) for y in x))
-        print()
-        ls = X['words']
-        print(ls.shape)
-        return ls
+    ngrams = pd.read_csv('../features/ngrams.csv', encoding='latin1')
+    scores = scores.merge(ngrams, on='rev_id')
+    del ngrams
 
-    def fit(self, X, y=None, **fit_params):
-        return self
+    offensiveness = pd.read_csv('../features/offensiveness_score.csv', encoding='latin1').iloc[0:n]
+    scores = scores.merge(offensiveness, on='rev_id')
+    del offensiveness
 
-print('Fitting pipeline')
-clf = Pipeline([
-    ('features', SimpleTransformerGeneral()),
-    ('LogisticRegression', LogisticRegression(class_weight='balanced'))
-    # ('RandomForest', RandomForestClassifier(n_estimators=1000, n_jobs=-1, verbose=0, class_weight='balanced', bootstrap=True))
-])
+    embeddings = pd.read_csv('../features/embeddings.csv', encoding='latin1').iloc[0:n]
+    linguistic = pd.read_csv('../features/linguistic.csv', encoding='latin1').iloc[0:n]
+    embeddings = embeddings.merge(linguistic, on='0')
+    del linguistic
 
-n = 100000
+    labels2 = pd.read_csv('../features/labels.csv', encoding='latin1')
+    new_labels = labels2.merge(scores, how='right', left_on='rev_id', right_on='rev_id')
+    del scores
 
-print('Getting data')
-print(datetime.datetime.now())
-labels2 = pd.read_csv('../features/labels_2_classes.csv', encoding='latin1')
-# partial = pd.read_csv('../data/partial.csv', encoding='latin1').drop(['Unnamed: 0'], axis=1)
+    new_labels = new_labels.merge(embeddings, left_on='rev_id', right_on='0').drop('0', axis=1)
+    del embeddings
 
-# comments = pd.read_pickle('../features/basic_comments_clean.txt').iloc[0:n]
-# print(comments.iloc[0])
-# ngrams = pd.read_csv('../features/ngrams3.csv', encoding='latin1')
-scores = pd.read_csv('../features/scores.csv', encoding='latin1').drop('Unnamed: 0', axis=1).iloc[0:n]
-# pos_tags = pd.read_csv('../features/pos_ngrams.csv', encoding='latin1').iloc[0:n]
-linguistic = pd.read_csv('../features/linguistic.csv', encoding='latin1').iloc[0:n]
-embeddings = pd.read_csv('../features/embeddings.csv', encoding='latin1').iloc[0:n]
+    features = new_labels.drop('label', axis=1).drop('rev_id', axis=1)
 
-print('Merging data')
-print(datetime.datetime.now())
+    return features, new_labels
 
-embeddings = embeddings.merge(linguistic, on='0')
 
-# ngrams = ngrams.merge(scores, on='rev_id')
-# scores = scores.merge(pos_tags, on='rev_id')
-# scores = scores.merge(ngrams, on='rev_id')
-# scores = scores.merge(comments, on='rev_id')
+def evaluate(labels_test, results):
+    print(classification_report(labels_test, results, target_names=['class_0', 'class_1', 'class_2', 'class_3', 'class_4', 'class_5']))
+    print(confusion_matrix(labels_test, results))
 
-new_labels = labels2.merge(scores, how='right', left_on='rev_id', right_on='rev_id')
-new_labels = new_labels.merge(embeddings, left_on='rev_id', right_on='0').drop('0', axis=1)
+    # auc = roc_auc_score(labels_test, results)
+    # print('Test ROC AUC: %.3f' % auc)
+    print(datetime.datetime.now())
 
-# features = scores.merge(linguistic, left_on='rev_id', right_on='0').drop('0', axis=1)
 
-# new_labels = labels2.merge(features, on='rev_id')
+n = 115615
+# n = 100
 
-features = new_labels.drop('label', axis=1).drop('rev_id', axis=1)
-
+features, new_labels = get_data(n)
 
 print('Splitting data')
 train, test, labels_train, labels_test = train_test_split(
     features, new_labels['label'],
     test_size=0.20, random_state=42)
 
-print('hi')
-print('Fitting and classifying')
-print(datetime.datetime.now())
-clf = clf.fit(train, labels_train)
-probabilities = clf.predict_proba(test)
-results = clf.predict(test)
-print(results)
+del features
+del new_labels
+# print('Fitting pipeline')
+# clf = Pipeline([
+#     ('features', SimpleTransformerGeneral()),
+#     # ('LogisticRegression', LogisticRegression(class_weight='balanced')),
+#     ('RandomForest', RandomForestClassifier(n_estimators=1000, n_jobs=-1, verbose=0, class_weight='balanced', bootstrap=True))
+# ])
 
-print(classification_report(labels_test, results, target_names=['class_0', 'class_1']))
-print(confusion_matrix(labels_test, results))
+models = []
+models.append(('LogisticRegression', LogisticRegression()))
+models.append(('LinearDiscriminantAnalysis', LinearDiscriminantAnalysis()))
+models.append(('DecisionTreeClassifier', DecisionTreeClassifier()))
+models.append(('GaussianNB', GaussianNB()))
+models.append(('RandomForestClassifier', RandomForestClassifier()))
+models.append(('ExtraTreesClassifier', ExtraTreesClassifier()))
+# models.append(('SVM', SVC()))
 
-auc = roc_auc_score(labels_test, results)
-print('Test ROC AUC: %.3f' %auc)
-print(datetime.datetime.now())
+for name, model in models:
+    clf = model
+    print(name)
+    print('Fitting and classifying')
+    print(datetime.datetime.now())
+    clf = clf.fit(train, labels_train)
+    results = clf.predict(test)
+    print(results)
+
+    evaluate(labels_test,  results)
+
